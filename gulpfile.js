8fef536ac6ba958b23d842c8cd18ec74436ae6c8
Kenneth Mwangi <kenmwangi4237@gmail.com>
2016-03-24 16:56:06 +0300

    file commited

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+
+/* include the plugins to use */
+var gulp=require('gulp'),
+    uglify=require('gulp-uglify'),
+    sass=require('gulp-ruby-sass');
+/* uglify the js using script task */
+/* minifies the code */
+gulp.task('script',function () {
+	gulp.src('js/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest('minjs'));
+});
+/* converting sass files to generate css style */
+gulp.task('style',function () {
+	//gulp.src('sass/*.scss')
+	return sass('sass/*.scss',{style:'compressed'})
+	.pipe(gulp.dest('css/'));
+});
+/* Automatic converting sass files to generate css style */
+gulp.task('watch',function () {
+	gulp.watch('sass/*.scss',['style']);
+});
+/* running the various tasks */
+gulp.task('default',['style','watch']);
